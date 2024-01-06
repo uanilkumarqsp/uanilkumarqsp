697d09ffbc42ed8fe90f975dc386af7d6982e1b4
Administrator <Administrator@DESKTOP-CJ2TM4F.mshome.net>
2024-01-06 12:31:27 +0530

    eng anilgit

---
+++ anilgit/src/test/java/anilgit/TC1.java
@@ -0,0 +1,13 @@
+package anilgit;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC1 {
+	@Test
+	public void tc1()
+	{
+		Reporter.log(" tc1 for collabaration ",true);
+	}
+
+}
